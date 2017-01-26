3d72d88d5f4ce6de3ff5b4a8e66a247e8f3a750a
vishalkr07 <vishalkr.k07@gmail.com>
2017-01-26 00:02:19 +0000

    initial commit

---
+++ login.php
@@ -0,0 +1,107 @@
+<?php
+
+//connect to database
+require 'include/connect.inc.php';
+session_start();
+if(isset($_SESSION['id'])){
+	header("location: home.php");
+}
+if (isset($_POST['username'])&&isset($_POST['password'])){
+	$username = trim(htmlentities($_POST['username']));
+	$password = trim(htmlentities($_POST['password']));
+	if(!empty($username)&&!empty($password)){
+		$password = md5($password);  //remember we hashed password before storing last time
+	  $sql = "SELECT * FROM users WHERE username = '$username' AND password='$password'";
+	  $result = mysqli_query($db, $sql);
+	  if(mysqli_num_rows($result) == 1)
+	  {
+			$row = mysqli_fetch_array($result);
+			//print_r($row);
+		  $_SESSION['username'] = $username;
+			$_SESSION['id'] = $row['id'];
+			//echo  $_SESSION['id'];
+			//echo $row['id'];
+
+		  $_SESSION['message'] = "you are now logged in";
+		  header("location: home.php"); //redirect to home page
+		  //echo 'OK';
+	  }
+	  else
+	  {
+		  $_SESSION['message'] = "Username/ password combination invalid";
+		}
+	} else {
+		$_SESSION['message'] = "Empty Field";
+	}
+}
+?>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Register, login and logout user php mqsql</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+		<link href="css/font-awesome.min.css" rel="stylesheet">
+		<link rel="stylesheet" type ="text/css" href="style.css">
+	</head>
+	<body>
+		<div class="header">
+			<h1>Register, login and logout user php mysql</h1>
+		</div>
+
+		 <?php
+		 if(isset($_SESSION['message'])){
+			 echo"<div id='error_msg'>". $_SESSION['message']."</div>";
+			 //unset($_SESSION['message']);
+		 } else {
+		 	echo "<div id = 'message' class = 'callout callout-info'>".'Please enter your username and password.'."</div>";
+		 }
+		 ?>
+
+		<div class="container">
+
+			<div class="row">
+				<div class="col-sm-4 col-sm-offset-4 col-xs-12">
+					<div class="row">
+
+					</div>
+					<div class="row">
+						<div id="msg" class="callout callout-info"><p>Please enter your username and password</p></div>
+					</div>
+					<div class="row">
+						<form id="login" data-error="<?php if(isset($_SESSION['message'])){ echo 'true'; unset($_SESSION['message']);} else {echo 'false';}?>" action="login.php" method="post">
+				        	<div class="form-group">
+				        		<label for="username" class="control-label" >Username</label>
+				          		<input id="username" name="username" class="form-control"  placeholder = "Username" type="text">
+								<p class="help-block"></p>
+				        	</div>
+				        	<div class="form-group">
+				          		<label for="password" class="control-label" >Password</label>
+				          		<input id="password" name="password" class="form-control" placeholder = "Password" type="text">
+								<p class="help-block"></p>
+				        	</div>
+				    		<button id="submit" value="Login" name="login_btn" type="submit"> Login</button>
+				     	</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		<script src="js/jquery.min.js" type="text/javascript"></script>
+    <script type="text/javascript">
+			$(document).ready(function () {
+				var elem = $("<i class='fa fa-times-circle-o'></i>");
+				//$('#password').closest('div').find('label').prepend(elem);
+				if ($('#login').data('error')){
+					var error = $(this).find('div:last');
+					$(error).find('label').prepend(elem);
+					$(error).addClass('login-error');
+				}
+			});
+		</script>
+
+	 </body>
+ </html>
---
+++ register.php
@@ -0,0 +1,114 @@
+
+<?php
+//connect to database
+require 'include/connect.inc.php';
+$signup_username_error = $signup_contact_error = $signup_password_error = $signup_email_error = "";
+if(!$db){
+	echo 'error';
+}
+if(isset($_POST['register_btn'])){
+	session_start();
+	$username = mysqli_real_escape_string($db,$_POST["username"]);
+	$Contact = mysqli_real_escape_string($db, $_POST["Contact"]);
+	$email = mysqli_real_escape_string($db,$_POST['email']);
+	$password = mysqli_real_escape_string($db,$_POST['password']);
+	$password2 = mysqli_real_escape_string($db,$_POST['password2']);
+
+
+           if(!empty($username)&&!empty($password) && !empty($email) && !empty($Contact)){
+            $signup=1;
+            if(strlen($username)>10){
+                $signup_username_error = ' Username must be less than 10 characters.';
+                $signup=0;
+            }
+			if(strlen($Contact)!=10)
+			{
+				$signup_contact_error = 'please enter valid contact number';
+				$signup=0;
+			}
+
+            if(strlen($password)>20||strlen($password)<8){
+                $signup_password_error = ' Password must be 8-20 characters.';
+                $signup=0;
+            }
+             if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+               $signup_email_error = "Invalid email format";
+                 $signup =0;
+			 }
+			 if($signup)
+			 {
+				 $sql = "SELECT username FROM users WHERE username= '$username'";
+				 $result = mysqli_query($db, $sql);
+				 $num = mysqli_num_rows($result);
+				 if($num!=0){
+					 die("there is user with that username");
+				 }
+				else{
+		            if($password == $password2)
+					{  //create user
+					$password = md5($password); // hash pashword before storing for secority purpose
+					$sql = "INSERT INTO users(username, email, password) VALUES('$username', '$email', '$password')";
+					mysqli_query($db, $sql);
+					$_SESSION['message'] = "You are now logged in" ;
+					$_SESSION['username'] = $username;
+					header("location: home.php");  // redirect to home apge
+
+	               }
+                   else
+					{
+					$_SESSION['message'] = "The two passwords do not match";
+
+					}
+}
+}
+}
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Register, login and logout user php mqsql</title>
+<link rel="stylesheet" type ="text/css" href="style.css">
+</head>
+<div class="header">
+ <h1>Register, login and logout user php mysql</h1>
+ </div>
+
+
+ <?php
+ if(isset($_SESSION['message'])){
+	 echo"<div id='error_msg'>". $_SESSION['message']."</div>";
+	 unset($_SESSION['message']);
+ }
+ ?>
+
+
+ <form method="post" action="register.php">
+ <table>
+ <tr>
+ <td>Username:</td>
+ <td><input type="text" name="username"  placeholder ="usename(1-10 character)" class="textInput"><?php echo $signup_username_error; ?></td>
+ </tr>
+ <tr>
+ <td>Contact no.</td>
+ <td><input type="text" name="Contact" placeholder="number(10 characters)" class="textInput"><?php echo $signup_conatact_error; ?></td>
+ </tr>
+ <tr>
+ <td>Email:</td>
+ <td><input type="email" name="email" class="textInput"><?php echo $signup_email_error; ?></td>
+ </tr>
+ <tr>
+ <td>Password:</td>
+ <td><input type="password" name="password"  placeholder="password(8-20 characters)" class="textInput"><?php echo $signup_password_error; ?></td>
+ </tr>
+ <tr>
+ <td>Confirm Password:</td>
+ <td><input type="password" name="password2" class="textInput"></td>
+ </tr>
+ <td><input type="submit" name="register_btn" value="Register"></td>
+ </tr>
+ </table>
+ </form>
+ </body>
+ </html>
