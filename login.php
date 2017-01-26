2e5c0e4b66ff8e74423c2f8c6bcd2c25f043da13
vishalkr07 <vishalkr.k07@gmail.com>
2017-01-26 11:33:09 +0000

    update register.php

--- login.php
+++ login.php
@@ -16,15 +16,11 @@ if (isset($_POST['username'])&&isset($_POST['password'])){
 	  if(mysqli_num_rows($result) == 1)
 	  {
 			$row = mysqli_fetch_array($result);
-			//print_r($row);
 		  $_SESSION['username'] = $username;
 			$_SESSION['id'] = $row['id'];
-			//echo  $_SESSION['id'];
-			//echo $row['id'];
 
 		  $_SESSION['message'] = "you are now logged in";
-		  header("location: home.php"); //redirect to home page
-		  //echo 'OK';
+		  header("location: home.php");
 	  }
 	  else
 	  {
@@ -42,9 +38,9 @@ if (isset($_POST['username'])&&isset($_POST['password'])){
 	<head>
 		<title>Register, login and logout user php mqsql</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<link href="css/bootstrap.min.css" rel="stylesheet">
+    	<link href="css/bootstrap-theme.min.css" rel="stylesheet">
 		<link href="css/font-awesome.min.css" rel="stylesheet">
 		<link rel="stylesheet" type ="text/css" href="style.css">
 	</head>
@@ -52,25 +48,14 @@ if (isset($_POST['username'])&&isset($_POST['password'])){
 		<div class="header">
 			<h1>Register, login and logout user php mysql</h1>
 		</div>
-
-		 <?php
-		 if(isset($_SESSION['message'])){
-			 echo"<div id='error_msg'>". $_SESSION['message']."</div>";
-			 //unset($_SESSION['message']);
-		 } else {
-		 	echo "<div id = 'message' class = 'callout callout-info'>".'Please enter your username and password.'."</div>";
-		 }
-		 ?>
-
 		<div class="container">
-
-			<div class="row">
+			<div class="row form-row">
 				<div class="col-sm-4 col-sm-offset-4 col-xs-12">
 					<div class="row">
 
 					</div>
 					<div class="row">
-						<div id="msg" class="callout callout-info"><p>Please enter your username and password</p></div>
+						<div id="msg" class="callout"><p></p></div>
 					</div>
 					<div class="row">
 						<form id="login" data-error="<?php if(isset($_SESSION['message'])){ echo 'true'; unset($_SESSION['message']);} else {echo 'false';}?>" action="login.php" method="post">
@@ -91,19 +76,20 @@ if (isset($_POST['username'])&&isset($_POST['password'])){
 			</div>
 		</div>
 		<script src="js/jquery.min.js" type="text/javascript"></script>
-    <script type="text/javascript">
+    	<script type="text/javascript">
 			$(document).ready(function () {
 				var callout = $('#msg');
 				var elem = $("<i class='fa fa-times-circle-o'></i>");
 				//$('#password').closest('div').find('label').prepend(elem);
 				if ($('#login').data('error')){
 					$(callout).addClass('callout-danger');
-					$(callout).find('p').text('hi');
+					$(callout).find('p').text('Invalid username or password.');
 					var error = $(this).find('div:last');
 					$(error).find('label').prepend(elem);
 					$(error).addClass('login-error');
 				} else {
-
+					$(callout).addClass('callout-info');
+					$(callout).find('p').text('Please enter your username and password');
 				}
 			});
 		</script>
--- register.php
+++ register.php
@@ -2,35 +2,30 @@
 <?php
 //connect to database
 require 'include/connect.inc.php';
+session_start();
 $signup_username_error = $signup_contact_error = $signup_password_error = $signup_email_error = "";
-if(!$db){
-	echo 'error';
-}
-if(isset($_POST['register_btn'])){
-	session_start();
-	$username = mysqli_real_escape_string($db,$_POST["username"]);
-	$Contact = mysqli_real_escape_string($db, $_POST["Contact"]);
-	$email = mysqli_real_escape_string($db,$_POST['email']);
-	$password = mysqli_real_escape_string($db,$_POST['password']);
-	$password2 = mysqli_real_escape_string($db,$_POST['password2']);
-
-
-           if(!empty($username)&&!empty($password) && !empty($email) && !empty($Contact)){
+if(isset($_POST['username'])&&isset($_POST['email'])&&isset($_POST['password'])){
+	$username = trim(mysqli_real_escape_string($db,$_POST["username"]));
+	$Contact = trim(mysqli_real_escape_string($db, $_POST["contact"]));
+	$email = trim(mysqli_real_escape_string($db,$_POST['email']));
+	$password = trim(mysqli_real_escape_string($db,$_POST['password']));
+	$password2 = trim(mysqli_real_escape_string($db,$_POST['password2']));
+    if(!empty($username)&&!empty($password) && !empty($email) && !empty($Contact)){
             $signup=1;
             if(strlen($username)>10){
                 $signup_username_error = ' Username must be less than 10 characters.';
                 $signup=0;
             }
-			if(strlen($Contact)!=10)
+			/*if(strlen($Contact)!=10)
 			{
 				$signup_contact_error = 'please enter valid contact number';
 				$signup=0;
-			}
+			}*/
 
-            if(strlen($password)>20||strlen($password)<8){
+            /*if(strlen($password)>20||strlen($password)<8){
                 $signup_password_error = ' Password must be 8-20 characters.';
                 $signup=0;
-            }
+            }*/
              if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
                $signup_email_error = "Invalid email format";
                  $signup =0;
@@ -49,9 +44,10 @@ if(isset($_POST['register_btn'])){
 					$password = md5($password); // hash pashword before storing for secority purpose
 					$sql = "INSERT INTO users(username, email, password) VALUES('$username', '$email', '$password')";
 					mysqli_query($db, $sql);
+					echo "OK";
 					$_SESSION['message'] = "You are now logged in" ;
 					$_SESSION['username'] = $username;
-					header("location: home.php");  // redirect to home apge
+					//header("location: home.php");  // redirect to home apge
 
 	               }
                    else
@@ -59,56 +55,87 @@ if(isset($_POST['register_btn'])){
 					$_SESSION['message'] = "The two passwords do not match";
 
 					}
-}
-}
-}
+		}
+		}
+	}
 }
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-<title>Register, login and logout user php mqsql</title>
-<link rel="stylesheet" type ="text/css" href="style.css">
-</head>
-<div class="header">
- <h1>Register, login and logout user php mysql</h1>
- </div>
+	<head>
+		<title>Register, login and logout user php mqsql</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<link href="css/bootstrap.min.css" rel="stylesheet">
+    	<link href="css/bootstrap-theme.min.css" rel="stylesheet">
+		<link href="css/font-awesome.min.css" rel="stylesheet">
+		<link rel="stylesheet" type ="text/css" href="style.css">
+	</head>
+	<body>
+	<div class="header">
+	 <h1>Register, login and logout user php mysql</h1>
+	 </div>
 
 
- <?php
- if(isset($_SESSION['message'])){
-	 echo"<div id='error_msg'>". $_SESSION['message']."</div>";
-	 unset($_SESSION['message']);
- }
- ?>
+	 <?php
+	 if(isset($_SESSION['message'])){
+		 echo"<div id='error_msg'>". $_SESSION['message']."</div>";
+		 unset($_SESSION['message']);
+	 }
+	 ?>
 
+	 <div class="container">
+		 <div class="row form-row">
+			 <div class="col-sm-4 col-sm-offset-4 col-xs-12">
+				 <div class="row">
 
- <form method="post" action="register.php">
- <table>
- <tr>
- <td>Username:</td>
- <td><input type="text" name="username"  placeholder ="usename(1-10 character)" class="textInput"><?php echo $signup_username_error; ?></td>
- </tr>
- <tr>
- <td>Contact no.</td>
- <td><input type="text" name="Contact" placeholder="number(10 characters)" class="textInput"><?php echo $signup_conatact_error; ?></td>
- </tr>
- <tr>
- <td>Email:</td>
- <td><input type="email" name="email" class="textInput"><?php echo $signup_email_error; ?></td>
- </tr>
- <tr>
- <td>Password:</td>
- <td><input type="password" name="password"  placeholder="password(8-20 characters)" class="textInput"><?php echo $signup_password_error; ?></td>
- </tr>
- <tr>
- <td>Confirm Password:</td>
- <td><input type="password" name="password2" class="textInput"></td>
- </tr>
- <td><input type="submit" name="register_btn" value="Register"></td>
- </tr>
- </table>
- </form>
+				 </div>
+				 <div class="row">
+					 <div id="msg" class="callout"><p></p></div>
+				 </div>
+				 <div class="row">
+					 <form id="signup" action="register.php" method="post">
+						 <div class="form-group">
+							 <label for="username" class="control-label" >Username</label>
+							 <input id="username" name="username" class="form-control"  placeholder = "Username" type="text">
+							 <p class="help-block"></p>
+						 </div>
+						 <div class="form-group">
+							 <label for="firstname" class="control-label" >Firstname</label>
+							 <input id="firstname" name="firstname" class="form-control" placeholder = "Firstname" type="text">
+							 <p class="help-block"></p>
+						 </div>
+						 <div class="form-group">
+							 <label for="lastname" class="control-label" >Lastname</label>
+							 <input id="lastname" name="lastname" class="form-control" placeholder = "Lastname" type="text">
+							 <p class="help-block"></p>
+						 </div>
+						 <div class="form-group">
+							 <label for="email" class="control-label" >Email</label>
+							 <input id="email" name="email" class="form-control" placeholder = "Email" type="text">
+							 <p class="help-block"></p>
+						 </div>
+						 <div class="form-group">
+							 <label for="password" class="control-label" >Password</label>
+							 <input id="password" name="password" class="form-control" placeholder = "Password" type="text">
+							 <p class="help-block"></p>
+						 </div>
+						 <div class="form-group">
+							 <label for="password2" class="control-label" >Confirm Password</label>
+							 <input id="password2" name="password2" class="form-control" placeholder = "Confirm Password" type="text">
+							 <p class="help-block"></p>
+						 </div>
+						 <div class="form-group">
+							 <label for="contact" class="control-label" >Contact No</label>
+							 <input id="contact" name="contact" class="form-control" placeholder = "Contact No" type="text">
+							 <p class="help-block"></p>
+						 </div>
+						 <button id="submit" value="register" name="register_btn" type="submit"> Login</button>
+					 </form>
+				 </div>
+			 </div>
+		 </div>
+	 </div>
  </body>
  </html>
