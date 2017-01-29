3d030ac6488ef3cb97e85d8ffe5d647d28e0453a
garg-Megha <meghagarg0907@gmail.com>
2017-01-29 23:43:16 +0530

    i add start.php  i modify detail.php

---
+++ start.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Patient Details</title>
+ <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<div class="header">
+<h1>Patient Details</h1>
+</div>
+
+<body>
+<h4> Details entered succesfully</h4>
+<div><a href="detail.php">Edit Patient Details</div>
+</body>
+</html>
